70ba9e1471a6ddc37162906807d9c1586210751a
Alexander Ananyevskiy <a.ananyevsky@agima.ru>
2022-02-13 11:21:44 +0300

    update settings

--- library/settings.py
+++ library/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
